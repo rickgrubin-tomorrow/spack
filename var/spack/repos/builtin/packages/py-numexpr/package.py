c8b3185ea42fb8cffce29ede73b1d5b2073d3b7c
Rick Grubin <rick.grubin@tomorrow.io>
2025-01-08 21:48:38 +0000

    bump acceptable numpy version

--- var/spack/repos/builtin/packages/py-numexpr/package.py
+++ var/spack/repos/builtin/packages/py-numexpr/package.py
@@ -34,7 +34,8 @@ class PyNumexpr(PythonPackage):
     depends_on("python@3.9:", when="@2.8.7:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
 
-    depends_on("py-numpy@1.13.3:1.25", type=("build", "run"), when="@2.8.3:")
+    #depends_on("py-numpy@1.13.3:1.25", type=("build", "run"), when="@2.8.3:")
+    depends_on("py-numpy@1.13.3:1.26", type=("build", "run"), when="@2.8.3:")
     # https://github.com/pydata/numexpr/issues/397
     depends_on("py-numpy@1.7:1.22", type=("build", "run"), when="@:2.7")
     # https://github.com/pydata/numexpr/pull/478
