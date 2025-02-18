de164e45ade2ed430326b6db6b771a0c86242f71
Alex Richert <alexander.richert@noaa.gov>
2025-02-18 12:12:42 -0800

    wgrib2: add cmake builder (#48447)
    * wgrib2: add cmake builder
    
    * wgrib2 add maintainer
    
    * [@spackbot] updating style on behalf of AlexanderRichert-NOAA
    
    * style fixes; add tar.gz for old vers
    
    * license update
    
    * wgrib2: don't restrict openjpeg variant by version
    
    * Update var/spack/repos/builtin/packages/wgrib2/package.py
    
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>
    
    * [@spackbot] updating style on behalf of AlexanderRichert-NOAA
    
    * Update package.py
    
    * Update package.py
    
    * Update var/spack/repos/builtin/packages/wgrib2/package.py
    
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>
    
    ---------
    
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- var/spack/repos/builtin/packages/wgrib2/package.py
+++ var/spack/repos/builtin/packages/wgrib2/package.py
@@ -45,8 +45,8 @@ class Wgrib2(MakefilePackage, CMakePackage):
     """Utility for interacting with GRIB2 files"""
 
     homepage = "https://www.cpc.ncep.noaa.gov/products/wesley/wgrib2"
-    url = "https://www.ftp.cpc.ncep.noaa.gov/wd51we/wgrib2/wgrib2.tgz.v2.0.8"
-    git = "https://github.com/NOAA-EMC/wgrib2"
+    url = "https://github.com/NOAA-EMC/wgrib2/archive/refs/tags/v3.5.0.tar.gz"
+    git = "https://github.com/NOAA-EMC/wgrib2.git"
 
     maintainers(
         "AlysonStahl-NOAA", "t-brown", "AlexanderRichert-NOAA", "Hang-Lei-NOAA", "edwardhartnett"
@@ -81,6 +81,9 @@ class Wgrib2(MakefilePackage, CMakePackage):
         sha256="d7f1a4f9872922c62b3c7818c022465532cca1f5666b75d3ac5735f0b2747793",
         extension="tar.gz",
     )
+
+    depends_on("c", type="build")
+    depends_on("fortran", type="build")
 
     def url_for_version(self, version):
         if version >= Version("3.2.0"):
