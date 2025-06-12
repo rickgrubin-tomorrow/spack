d5ff23b167b49861dc4a5bf3b0148f23d730596f
Test User <spack@example.com>
2025-06-12 14:00:24 -0600

    Add esmf@8.9.0b11

--- var/spack/repos/builtin/packages/esmf/package.py
+++ var/spack/repos/builtin/packages/esmf/package.py
@@ -31,6 +31,7 @@ class Esmf(MakefilePackage, PythonExtension):
     # Develop is a special name for spack and is always considered the newest version
     version("develop", branch="develop")
     # generate chksum with 'spack checksum esmf@x.y.z'
+    version("8.9.0b11", commit="c27280964764a0e1258ff9faab6bf4e2fac39406")
     version("8.9.0b09", commit="7e8c4b2f21bbea6432a3dadd6e6a7e143b786193")
     version("8.9.0b08", commit="56e26fde7b4890ae44eb3efdf2f6728b5eb4627e")
     version("8.9.0b05", commit="5df19ab277b4517a093af0510f08171c478ec627")
